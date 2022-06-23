06ecfc6b9a891db3384362b2af02c0f07e899a94
Esteban Pisani <estebandpisani@gmail.com>
2022-06-23 12:30:20 -0300

    Home Carousel with redux

--- frontend/src/components/CarouselSection.jsx
+++ frontend/src/components/CarouselSection.jsx
@@ -1,37 +1,40 @@
 import React from "react";
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Carousel from './Carousel'
-import axios from 'axios';
 import '../styles/Carousel.css';
 import ReactLoading from 'react-loading';
-// import data from '../data'
+
+import { useDispatch, useSelector } from 'react-redux';
+import cityActions from '../redux/actions/cityActions';
 
 const CarouselSection = () => {
 
-    const [cities, setCities] = useState();
     const carouselBg = process.env.PUBLIC_URL + '/img/home-carrousel.jpg';
 
+    const dispatch = useDispatch();
+
     useEffect(() => {
-        axios.get('http://localhost:4000/api/cities')
-            .then(APIresp => {
-                setCities(APIresp.data.response.cities.slice(0, 12));
-            });
+        dispatch(cityActions.getCities());
     }, []);
+
+        let cities = useSelector(store => store.cityReducer.cities.slice(0, 12));
+        console.log(cities);
+
     return (
         <Box className='carrousel-section' sx={{ backgroundImage: `url(${carouselBg})`, height: { xs: '180vh', sm: '100vh' } }}>
             <Typography className='font-weird text-primary text-shadow-blur-light' sx={{ fontSize: '3rem' }}> Popular MyTineraries!</Typography>
             <Box className='carrousel-container'>
-            {cities ? (            
-                    <Carousel cities={cities} />               
+                {cities.length >0 ? (
+                    <Carousel cities={cities} />
                 ) : (
-                <Box className='skeleton-container' sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-evenly', flexDirection: 'column', height: '50vh' }} >
-                    <ReactLoading type={"spokes"} color={"#fff"} height={120} width={120} />
-                    <Typography variant='h4' className='text-light font-slogan'>Loading cities...</Typography>
-                </Box>
+                    <Box className='skeleton-container' sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-evenly', flexDirection: 'column', height: '50vh' }} >
+                        <ReactLoading type={"spokes"} color={"#fff"} height={120} width={120} />
+                        <Typography variant='h4' className='text-light font-slogan'>Loading cities...</Typography>
+                    </Box>
                 )
-            }
+                }
             </Box>
         </Box >
     )
